7c947700f54f5d4a95e5ae2487af278e80883e4f
Sejin Kim <kim3@kenyon.edu>
2021-02-21 00:35:12 +0000

    Edited the filepath for the conversion script

--- scripts/createcsv.R
+++ scripts/createcsv.R
@@ -3,7 +3,7 @@
 ### STAT 306 S21 @ Kenyon College
 
 ### Read RDS and output a CSV
-alldata <- readRDS(file = "nflfastr_pbp_2010_to_2020.rds")
+alldata <- readRDS(file = "/home/kim3/nfl_analysis_data/nflfastr_pbp_2010_to_2020.rds")
 
 ### Output CSV
-write.csv(alldata, file = "nflfastr_pbp_2010_to_2020.csv")
+write.csv(alldata, file = "../nflfastr_pbp_2010_to_2020.csv")
